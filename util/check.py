d182260310b27b8dbed8482875c3080a6d8e933a
Spencer-Clay <spencer_clay@foxmail.com>
2022-02-11 16:49:42 +0800

    修改代码

--- util/check.py
+++ util/check.py
@@ -1,7 +1,7 @@
 from multiprocessing import Pool
 import requests
-import util.writeFile
-
+import writeFile
+import os
 WORKERS = 10
 
 
@@ -14,14 +14,16 @@ def check(url):
     try:
         r = requests.get(check_url, proxies=proxies, timeout=1)
         if r.status_code == 200:
-            writeFile.writeFile("../alive_host.txt", url)
+            writeFile.writeFile("../alive_host.txt", url + '\n')
     except Exception:
         pass
     pass
 
 
 if __name__ == "__main__":
-    with open("../proxylist.txt") as targets:
+    if os.path.exists("../alive_host.txt"):
+        os.remove("../alive_host.txt")
+    with open("../proxy_list.txt") as targets:
         targets = targets.read().splitlines()
     with Pool(processes=WORKERS) as executor:
         executor.map(check, targets)
